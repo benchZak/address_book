e264eab8cc65f67c93e50ab730e8d190554618a0
benchZak <101592146+benchZak@users.noreply.github.com>
2022-03-29 14:06:21 -0400

    Update addressbook.js

--- scripts/addressbook.js
+++ scripts/addressbook.js
@@ -34,16 +34,12 @@ window.onload = function(){
 	
 	function ValidateEmail(input) {
   		var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
- 		 if (input.value.match(validRegex)) {
-  			alert("Valid email address!");
-   			document.form1.text1.focus();
-  		  return true;
-		  } else {
-   			 alert("Invalid email address!");
-   			 document.form1.text1.focus();
-   			 return false;
-  			}
-	}
+ 		 if (input.value.match(validRegex)) 
+  		  	return true;
+		 else 
+   			return false;
+	}	
+	
 
 
 	function addToBook(){
